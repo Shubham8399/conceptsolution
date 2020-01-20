bee69a2f7f0cabfdead01fb49cd9b2f3a509e593
Shubham <ptdrshubham8349@gmail.com>
2020-01-20 18:01:14 +0530

    homecontrollerwork

--- app/Http/Controllers/Homecontroller.php
+++ app/Http/Controllers/Homecontroller.php
@@ -45,7 +45,7 @@ class Homecontroller extends Controller
             'password'=>'required|string|min:6',
              ]);
 
-            
+
             $first_name = $request->input('first_name');
             $last_name = $request->input('last_name');
             $mobile = $request->input('mobile');
@@ -53,64 +53,68 @@ class Homecontroller extends Controller
             $password = $request->input('password');
             $token=sha1(time());
             print_r($first_name);
-          
+
            $data=array('first_name'=>$first_name,"last_name"=>$last_name,"mobile"=>$mobile,"email"=>$email,"password"=>$password,"token"=>$token);
         print_r($data);
-        die();
-        if($data > 0)
-        {
-        $insert=DB::table('user')->insert($data);
-         $post = array("S"=>"1", "message"=>"Success", 'id'=>$insert);
 
-        }
-        else{
-            $post = array('S'=>"0", "message"=>"Missing parameter");
-        }
-                return response()->json([$post], 201);
-        }
-        // Do LOGIN SHOW
-       public function dologin(Request $request){
-        $request->validate([
-            'email'=> 'required|string|email|max:191|unique:users',
-            'password'=>'required|string|min:6',
-        ]);
-        $email = $request->input('email');
-        $password = $request->input('password');
-        $data=array('email'=>$email,'password'=>$password);
-        if($data>0)
-        {
-            $users = DB::table('user')->get();
-            $post = array('S'=>"1", "token"=>$users[0]);
-
-        }
-        else{
-            $post = array("S"=>"2", "message"=>"Invalid Login ID or Password");
-        }
-          echo json_encode($post);
        }
-      // SHOW CATEGORY
-      public function showcategory(){
-        return view('category');
-      }
+      //   die();
+      //   if($data > 0)
+      //   {
+      //   $insert=DB::table('user')->insert($data);
+      //    $post = array("S"=>"1", "message"=>"Success", 'id'=>$insert);
 
-      // SHOW PRODUCT DETAILS
+      //   }
+      //   else{
+      //       $post = array('S'=>"0", "message"=>"Missing parameter");
+      //   }
+      //           return response()->json([$post], 201);
+      //   }
+      //   // Do LOGIN SHOW
+      //  public function dologin(Request $request){
+      //   $request->validate([
+      //       'email'=> 'required|string|email|max:191|unique:users',
+      //       'password'=>'required|string|min:6',
+      //   ]);
+      //   $email = $request->input('email');
+      //   $password = $request->input('password');
+      //   $data=array('email'=>$email,'password'=>$password);
+      //   if($data>0)
+      //   {
+      //       $users = DB::table('user')->get();
+      //       $post = array('S'=>"1", "token"=>$users[0]);
 
-      public function showdetails(){
-          return view('details');
-      }
+      //   }
+      //   else{
+      //       $post = array("S"=>"2", "message"=>"Invalid Login ID or Password");
+      //   }
+      //     echo json_encode($post);
+      //  }
+      // // SHOW CATEGORY
+      // public function showcategory(){
+      //   return view('category');
+      // }
 
-      public function show_product(){
+      // // SHOW PRODUCT DETAILS
 
-        $users = DB::table('tbl_product')->get();
-        $post = array('S'=>"1", "products"=>$users[0]);
-        echo json_encode($post);
-      }
+      // public function showdetails(){
+      //     return view('details');
+      // }
 
-      // SHOW CHECKOUT 
+      // public function show_product(){
 
-      public function show_checkout(){
-         return view('checkout');
-      }
+      //   $users = DB::table('tbl_product')->get();
+      //   $post = array('S'=>"1", "products"=>$users[0]);
+      //   echo json_encode($post);
+      // }
+
+      // // SHOW CHECKOUT
+
+      // public function show_checkout(){
+      //    return view('checkout');
+      // }
+
+
 
 
 
